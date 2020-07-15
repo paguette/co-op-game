2f0e2f8b12a9653338be37b46a1f922412f29d95
stext <stext@kek.com>
2020-07-15 23:00:18 +0300

    add basic player controller

---
+++ co-op-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 1;
+    private Rigidbody2D playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        playerRigidbody.velocity = (movement * playerSpeed);
+    }
+}
